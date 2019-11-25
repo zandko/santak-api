efeeba8d1aa5841ffdd632a149d752bf6b02a565
gaozan <root@LAPTOP-LI328TRL.localdomain>
2019-11-25 10:56:21 +0800

    修改shell

--- setup.sh
+++ setup.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 #image_version=`date +%Y%m%d%H%M`;
 
-cd /usr/src/node-app/egg-santak
-
 docker-compose stop || true;
 docker-compose down || true;
 
-docker rmi --force $(docker images | grep nodejs | awk '{print $3}')
+# docker rmi --force $(docker images | grep nodejs | awk '{print $3}')
 
-docker-compose build .;
+docker-compose build;
 
 docker-compose up -d;
 
