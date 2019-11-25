553bfa67d73aeb4c39ffccccf6a6224ad86a1a04
gaozan <root@LAPTOP-LI328TRL.localdomain>
2019-11-25 10:04:31 +0800

    fix

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ cd /usr/src/node-app/egg-santak
 docker-compose stop || true;
 docker-compose down || true;
 
+docker rmi --force $(docker images | grep nodejs | awk '{print $3}')
+
 docker-compose build .;
 
 docker-compose up -d;
